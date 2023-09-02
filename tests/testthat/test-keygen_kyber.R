33f7d2910fc9c4963b1e782fd79ca74454a393d9
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2023-09-02 15:13:10 +0200

    Slight change to keypair structure.
    Renamed 'secret' key as 'private' key.

--- tests/testthat/test-keygen_kyber.R
+++ tests/testthat/test-keygen_kyber.R
@@ -4,7 +4,7 @@ test_that("Keys generation", {
   k <- keygen_kyber(512)
 
   expect_s3_class(k, "keypair")
-  expect_equal(length(k$secret), 1632)
+  expect_equal(length(k$private), 1632)
   expect_equal(length(k$public), 800)
 
 })
