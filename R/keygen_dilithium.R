b5fe71c8cb9c7254b058147b4978e3b5c066d289
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2023-09-04 22:38:18 +0200

    Dilithium keygen full implementation.

--- R/keygen_dilithium.R
+++ R/keygen_dilithium.R
@@ -2,8 +2,10 @@
 
 #' Dilithium Key Generation
 #'
-#' @param version  Type of key to be generated. Use 2 for Dilithium2, 3 for Dilithium3
-#' or 5 for Dilithium5.
+#' @param strength  Type of key strength to be generated. Different strengths are available.
+#' Use 2 for Dilithium2 (claimed strength category 2), 3 for Dilithium3 (default, claimed strength
+#' category 3) or 5 for Dilithium5 (claimed strength category 5). For more information about
+#' strength categories see the vignette.
 #'
 #' @return A `keypair` object.
 #' @export
@@ -12,21 +14,28 @@
 #' key <- keygen_dilithium()
 #' key$key_type
 #'
-keygen_dilithium <- function(version=2) {
+keygen_dilithium <- function(strength = 3) {
 
-
-  key <- cpp_keygen_dilithium2()
+  if (strength == 2) {
+    key <- cpp_keygen_dilithium2()
+  } else if (strength == 3) {
+    key <- cpp_keygen_dilithium3()
+  } else if (strength == 5) {
+    key <- cpp_keygen_dilithium5()
+  } else {
+    stop("Unknown 'strength'. Acceptable values: 2, 3 or 5.")
+  }
 
   keypair <- list(key_type = "dilithium",
-                  parameters = version,
+                  strength = strength,
                   private = structure(key[[1]],
                                       key_type = "dilithium",
-                                      param = version,
-                                      class="private_key"),
+                                      strength = strength,
+                                      class = "private_key"),
                   public = structure(key[[2]],
-                                     key_type="dilithium",
-                                     param = version,
-                                     class="public_key"))
+                                     key_type = "dilithium",
+                                     strength = strength,
+                                     class = "public_key"))
   class(keypair) <- c("keypair", "dilithium")
 
   rm(key)
