fef536442d25c0556e182331a8f1424bc2358067
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2023-09-13 20:32:45 +0200

    Add tests to Sphincs+ SHA2 key generation, signature and validation

--- tests/testthat/test-verify_sphincs.R
+++ tests/testthat/test-verify_sphincs.R
@@ -112,6 +112,121 @@ test_that("Sphincs+ signature validation (shake, 256, fast)", {
   expect_false(verify_sphincs(important_message, forged_signature, key$public))
 })
 
+test_that("Sphincs+ signature validation (sha2, 128, small)", {
+
+  key <- keygen_sphincs(hash_type = "sha2", category = 128, type = "small")
+  important_message <- "Hello world!!"
+  signature <- sign_sphincs(key$private, important_message)
+
+  expect_true(verify_sphincs(important_message, signature, key$public))
+
+  expect_false(verify_sphincs("tampered_message", signature, key$public))
+
+  forged_signature <- signature
+  if (forged_signature[1] != as.raw(0L)) {
+    forged_signature[1] <- as.raw(0L)
+  } else {
+    forged_signature[1] <- as.raw(255L)
+  }
+  expect_false(verify_sphincs(important_message, forged_signature, key$public))
+})
+
+test_that("Sphincs+ signature validation (sha2, 128, fast)", {
+
+  key <- keygen_sphincs(hash_type = "sha2", category = 128, type = "fast")
+  important_message <- "Hello world!!"
+  signature <- sign_sphincs(key$private, important_message)
+
+  expect_true(verify_sphincs(important_message, signature, key$public))
+
+  expect_false(verify_sphincs("tampered_message", signature, key$public))
+
+  forged_signature <- signature
+  if (forged_signature[1] != as.raw(0L)) {
+    forged_signature[1] <- as.raw(0L)
+  } else {
+    forged_signature[1] <- as.raw(255L)
+  }
+  expect_false(verify_sphincs(important_message, forged_signature, key$public))
+})
+
+test_that("Sphincs+ signature validation (sha2, 192, small)", {
+
+  key <- keygen_sphincs(hash_type = "sha2", category = 192, type = "small")
+  important_message <- "Hello world!!"
+  signature <- sign_sphincs(key$private, important_message)
+
+  expect_true(verify_sphincs(important_message, signature, key$public))
+
+  expect_false(verify_sphincs("tampered_message", signature, key$public))
+
+  forged_signature <- signature
+  if (forged_signature[1] != as.raw(0L)) {
+    forged_signature[1] <- as.raw(0L)
+  } else {
+    forged_signature[1] <- as.raw(255L)
+  }
+  expect_false(verify_sphincs(important_message, forged_signature, key$public))
+})
+
+test_that("Sphincs+ signature validation (sha2, 192, fast)", {
+
+  key <- keygen_sphincs(hash_type = "sha2", category = 192, type = "fast")
+  important_message <- "Hello world!!"
+  signature <- sign_sphincs(key$private, important_message)
+
+  expect_true(verify_sphincs(important_message, signature, key$public))
+
+  expect_false(verify_sphincs("tampered_message", signature, key$public))
+
+  forged_signature <- signature
+  if (forged_signature[1] != as.raw(0L)) {
+    forged_signature[1] <- as.raw(0L)
+  } else {
+    forged_signature[1] <- as.raw(255L)
+  }
+  expect_false(verify_sphincs(important_message, forged_signature, key$public))
+})
+
+test_that("Sphincs+ signature validation (sha2, 256, small)", {
+
+  key <- keygen_sphincs(hash_type = "sha2", category = 256, type = "small")
+  important_message <- "Hello world!!"
+  signature <- sign_sphincs(key$private, important_message)
+
+  expect_true(verify_sphincs(important_message, signature, key$public))
+
+  expect_false(verify_sphincs("tampered_message", signature, key$public))
+
+  forged_signature <- signature
+  if (forged_signature[1] != as.raw(0L)) {
+    forged_signature[1] <- as.raw(0L)
+  } else {
+    forged_signature[1] <- as.raw(255L)
+  }
+  expect_false(verify_sphincs(important_message, forged_signature, key$public))
+})
+
+test_that("Sphincs+ signature validation (sha2, 256, fast)", {
+
+  key <- keygen_sphincs(hash_type = "sha2", category = 256, type = "fast")
+  important_message <- "Hello world!!"
+  signature <- sign_sphincs(key$private, important_message)
+
+  expect_true(verify_sphincs(important_message, signature, key$public))
+
+  expect_false(verify_sphincs("tampered_message", signature, key$public))
+
+  forged_signature <- signature
+  if (forged_signature[1] != as.raw(0L)) {
+    forged_signature[1] <- as.raw(0L)
+  } else {
+    forged_signature[1] <- as.raw(255L)
+  }
+  expect_false(verify_sphincs(important_message, forged_signature, key$public))
+})
+
+
 test_that("Sphincs+ digital signatures verification fails with bad parameters", {
 
   key <- keygen_sphincs()
