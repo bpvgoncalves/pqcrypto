913dbca2b9b7731eb845453c31a5e10a91546f3d
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2025-10-07 00:14:44 +0200

    chore: update ML-KEM (Kyber) sources from pq-crystals repository

--- src/kyber/kem.c
+++ src/kyber/kem.c
@@ -78,7 +78,7 @@ int crypto_kem_enc_derand(uint8_t *ct,
                           const uint8_t *pk,
                           const uint8_t *coins)
 {
-  uint8_t buf[2*KYBER_SYMBYTES+1];
+  uint8_t buf[2*KYBER_SYMBYTES];
   /* Will contain key, coins */
   uint8_t kr[2*KYBER_SYMBYTES];
 
@@ -86,8 +86,6 @@ int crypto_kem_enc_derand(uint8_t *ct,
 
   /* Multitarget countermeasure for coins + contributory KEM */
   hash_h(buf+KYBER_SYMBYTES, pk, KYBER_PUBLICKEYBYTES);
-  /* Add byte separating Kyber parameter sets */
-  buf[2*KYBER_SYMBYTES] = KYBER_K;
   hash_g(kr, buf, 2*KYBER_SYMBYTES);
 
   /* coins are in kr+KYBER_SYMBYTES */
@@ -147,7 +145,8 @@ int crypto_kem_dec(uint8_t *ss,
   uint8_t buf[2*KYBER_SYMBYTES];
   /* Will contain key, coins */
   uint8_t kr[2*KYBER_SYMBYTES];
-  uint8_t cmp[KYBER_CIPHERTEXTBYTES+KYBER_SYMBYTES];
+//  uint8_t cmp[KYBER_CIPHERTEXTBYTES+KYBER_SYMBYTES];
+  uint8_t cmp[KYBER_CIPHERTEXTBYTES];
   const uint8_t *pk = sk+KYBER_INDCPA_SECRETKEYBYTES;
 
   indcpa_dec(buf, ct, sk);
